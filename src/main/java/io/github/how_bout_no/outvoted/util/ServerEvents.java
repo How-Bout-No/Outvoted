3a4acc53f2620e205e97f71e6701bac533ef68e9
Noah <thefalconpunch@icloud.com>
2021-01-27 22:25:14 -0500

    Make logs strippable

--- src/main/java/io/github/how_bout_no/outvoted/util/ServerEvents.java
+++ src/main/java/io/github/how_bout_no/outvoted/util/ServerEvents.java
@@ -1,24 +1,35 @@
 package io.github.how_bout_no.outvoted.util;
 
+import io.github.how_bout_no.outvoted.Outvoted;
 import io.github.how_bout_no.outvoted.entity.InfernoEntity;
 import io.github.how_bout_no.outvoted.init.ModEntityTypes;
 import io.github.how_bout_no.outvoted.init.ModItems;
 import io.github.how_bout_no.outvoted.item.InfernoShieldItem;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.RotatedPillarBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.AxeItem;
 import net.minecraft.item.Item;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber(modid = Outvoted.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ServerEvents {
 
-    /**
-     * Sets entities that attack a player blocking with an Inferno Shield on fire
-     */
     @SubscribeEvent
     public void onLivingAttacked(LivingAttackEvent event) {
+        /* Sets entities that attack a player blocking with an Inferno Shield on fire */
         if (event.getSource().getTrueSource() != null) {
             Entity attacker = event.getSource().getTrueSource();
             LivingEntity player = event.getEntityLiving();
@@ -35,11 +46,9 @@ public class ServerEvents {
         }
     }
 
-    /**
-     * Sets variant texture tags for Inferno Helmet
-     */
     @SubscribeEvent
     public void onEntityDrops(LivingDropsEvent event) {
+        /* Sets variant texture nbt tags for Inferno Helmet */
         if (event.getEntity().getType() == ModEntityTypes.INFERNO.get()) {
             InfernoEntity entity = (InfernoEntity) event.getEntityLiving();
             ItemEntity helmet = event.getDrops().stream().filter(item -> item.getItem().getItem() == ModItems.INFERNO_HELMET.get()).findFirst().orElse(null);
@@ -51,27 +60,14 @@ public class ServerEvents {
         }
     }
 
-    /*
-
-    //Wood stripping event
-    //copied from https://forums.minecraftforge.net/topic/83842-1144-stripped-logs-help-solved/?do=findComment&comment=396477
-
-    public static Map<Block, Block> BLOCK_STRIPPING_MAP = new HashMap<>();
-
-
-    //idk how to do this part
-    static {
-        BLOCK_STRIPPING_MAP.put(Registry.PALM_LOG, Registry.STRIPPED_PALM_LOG);
-        BLOCK_STRIPPING_MAP.put(Registry.PALM_WOOD, Registry.STRIPPED_PALM_WOOD);
-    }
-
     @SubscribeEvent
     public static void onBlockClicked(PlayerInteractEvent.RightClickBlock event) {
+        /* Allows stripping of logs */
         if (event.getItemStack().getItem() instanceof AxeItem) {
             World world = event.getWorld();
             BlockPos blockpos = event.getPos();
             BlockState blockstate = world.getBlockState(blockpos);
-            Block block = BLOCK_STRIPPING_MAP.get(blockstate.getBlock());
+            Block block = BlockUtils.Stripping.BLOCK_STRIPPING_MAP.get(blockstate.getBlock());
             if (block != null) {
                 PlayerEntity playerentity = event.getPlayer();
                 world.playSound(playerentity, blockpos, SoundEvents.ITEM_AXE_STRIP, SoundCategory.BLOCKS, 1.0F, 1.0F);
@@ -86,8 +82,5 @@ public class ServerEvents {
                 }
             }
         }
-
     }
-
-     */
 }
