ebba19f462ffbe731d54b2c3e9968156f389556e
Noah <thefalconpunch@icloud.com>
2021-05-13 06:28:22 -0400

    Replace terrible disgusting awful mixin with Forge method The mixin was only there cause I thought method didn't work, but it does, just not in dev

---
+++ forge/src/main/java/io/github/how_bout_no/outvoted/item/WildfireShieldItem.java
@@ -0,0 +1,26 @@
+package io.github.how_bout_no.outvoted.item;
+
+import io.github.how_bout_no.outvoted.Outvoted;
+import io.github.how_bout_no.outvoted.OutvotedModPlatform;
+import io.github.how_bout_no.outvoted.util.GroupCheck;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ShieldItem;
+import org.jetbrains.annotations.Nullable;
+
+public class WildfireShieldItem extends ShieldItem {
+    public WildfireShieldItem() {
+        super(OutvotedModPlatform.setISTER(new Settings().maxDamage(750).fireproof().group(Outvoted.TAB_COMBAT)));
+    }
+
+    @Override
+    public boolean isIn(ItemGroup group) {
+        return GroupCheck.isIn(group, Outvoted.TAB_COMBAT);
+    }
+
+    @Override
+    public boolean isShield(ItemStack stack, @Nullable LivingEntity entity) {
+        return true;
+    }
+}
