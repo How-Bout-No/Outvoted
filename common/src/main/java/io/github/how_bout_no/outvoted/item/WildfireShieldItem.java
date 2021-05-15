9216e7d98e4f63553592f28730afb6c970a276e5
Noah <thefalconpunch@icloud.com>
2021-05-15 08:21:18 -0400

    Just put isShield in normal WildfireShieldItem

--- common/src/main/java/io/github/how_bout_no/outvoted/item/WildfireShieldItem.java
+++ common/src/main/java/io/github/how_bout_no/outvoted/item/WildfireShieldItem.java
@@ -3,9 +3,13 @@ package io.github.how_bout_no.outvoted.item;
 import io.github.how_bout_no.outvoted.Outvoted;
 import io.github.how_bout_no.outvoted.OutvotedModPlatform;
 import io.github.how_bout_no.outvoted.util.GroupCheck;
+import me.shedaniel.architectury.annotations.PlatformOnly;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ShieldItem;
+import org.jetbrains.annotations.Nullable;
 
 public class WildfireShieldItem extends ShieldItem {
     public WildfireShieldItem() {
@@ -15,5 +19,10 @@ public class WildfireShieldItem extends ShieldItem {
     @Override
     protected boolean isIn(ItemGroup group) {
         return GroupCheck.isIn(group, Outvoted.TAB_COMBAT);
+    }
+
+    @PlatformOnly("forge")
+    public boolean isShield(ItemStack stack, @Nullable LivingEntity entity) {
+        return true;
     }
 }
