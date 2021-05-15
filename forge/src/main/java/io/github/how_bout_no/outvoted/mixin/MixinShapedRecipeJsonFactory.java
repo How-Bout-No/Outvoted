30ef33ee77f0db9b98aba92634cb7746fa32a720
Noah <thefalconpunch@icloud.com>
2021-05-15 08:20:51 -0400

    I guess the mixins fix themselves?

--- forge/src/main/java/io/github/how_bout_no/outvoted/mixin/MixinShapedRecipeJsonFactory.java
+++ forge/src/main/java/io/github/how_bout_no/outvoted/mixin/MixinShapedRecipeJsonFactory.java
@@ -19,7 +19,7 @@ public abstract class MixinShapedRecipeJsonFactory {
     @Redirect(method = "offerTo(Ljava/util/function/Consumer;Lnet/minecraft/util/Identifier;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemGroup;getName()Ljava/lang/String;"))
     private String bruh(ItemGroup itemGroup) {
         for (ItemGroup group : Outvoted.TABS) {
-            if (((ItemInvoker) this.output).invokeIsIn(group)) {
+            if (this.output.isIn(group)) {
                 return group.getName();
             }
         }
