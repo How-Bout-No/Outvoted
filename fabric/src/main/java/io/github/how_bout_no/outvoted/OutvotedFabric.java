e9cd4048e452375f898fdacb44711c9118573565
Noah <thefalconpunch@icloud.com>
2021-05-31 17:19:01 -0400

    Use Fabrics spawn restriction so stuff doesnt crash (maybe fix #51)

--- fabric/src/main/java/io/github/how_bout_no/outvoted/OutvotedFabric.java
+++ fabric/src/main/java/io/github/how_bout_no/outvoted/OutvotedFabric.java
@@ -1,11 +1,15 @@
 package io.github.how_bout_no.outvoted;
 
-import io.github.how_bout_no.outvoted.init.ModFeatures;
-import io.github.how_bout_no.outvoted.init.ModFireBlock;
-import io.github.how_bout_no.outvoted.init.ModPOITypes;
-import io.github.how_bout_no.outvoted.init.ModSigns;
+import io.github.how_bout_no.outvoted.entity.BarnacleEntity;
+import io.github.how_bout_no.outvoted.entity.GluttonEntity;
+import io.github.how_bout_no.outvoted.entity.MeerkatEntity;
+import io.github.how_bout_no.outvoted.init.*;
 import io.github.how_bout_no.outvoted.item.ModSpawnEggItem;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.mixin.object.builder.SpawnRestrictionAccessor;
+import net.minecraft.entity.SpawnRestriction;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.world.Heightmap;
 
 public class OutvotedFabric implements ModInitializer {
     @Override
@@ -16,5 +20,17 @@ public class OutvotedFabric implements ModInitializer {
         ModFireBlock.init();
         ModSigns.init();
         ModPOITypes.POINT_OF_INTEREST_TYPES.register();
+        spawns();
+    }
+
+    public void spawns() {
+        SpawnRestrictionAccessor.callRegister(ModEntityTypes.WILDFIRE.get(), SpawnRestriction.Location.ON_GROUND,
+                Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, HostileEntity::canSpawnInDark);
+        SpawnRestrictionAccessor.callRegister(ModEntityTypes.GLUTTON.get(), SpawnRestriction.Location.ON_GROUND,
+                Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, GluttonEntity::canSpawn);
+        SpawnRestrictionAccessor.callRegister(ModEntityTypes.BARNACLE.get(), SpawnRestriction.Location.IN_WATER,
+                Heightmap.Type.MOTION_BLOCKING, BarnacleEntity::canSpawn);
+        SpawnRestrictionAccessor.callRegister(ModEntityTypes.MEERKAT.get(), SpawnRestriction.Location.ON_GROUND,
+                Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, MeerkatEntity::canSpawn);
     }
 }
