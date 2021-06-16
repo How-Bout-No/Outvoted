71ff4c25c891c7c8d2d4e3592b9c774c8f27a0f3
Noah <thefalconpunch@icloud.com>
2021-06-16 12:02:30 -0400

    Remove redundant stuff

--- common/src/main/java/io/github/how_bout_no/outvoted/mixin/MixinAnvilScreenHandler.java
+++ common/src/main/java/io/github/how_bout_no/outvoted/mixin/MixinAnvilScreenHandler.java
@@ -14,26 +14,17 @@ import net.minecraft.screen.ScreenHandlerType;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AnvilScreenHandler.class)
 public abstract class MixinAnvilScreenHandler extends ForgingScreenHandler {
-    private ItemStack stack;
-
     public MixinAnvilScreenHandler(@Nullable ScreenHandlerType<?> type, int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
         super(type, syncId, playerInventory, context);
     }
 
-    @Inject(method = "updateResult", at = @At("HEAD"))
-    private void getLocal(CallbackInfo ci) {
-        stack = this.input.getStack(0);
-    }
-
     @Redirect(method = "updateResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;"))
     private Item overwriteResult(ItemStack itemStack) {
-        if (stack.getItem() instanceof WildfireHelmetItem && itemStack.getItem() == Items.ENCHANTED_BOOK) {
+        if (this.input.getStack(0).getItem() instanceof WildfireHelmetItem && itemStack.getItem() == Items.ENCHANTED_BOOK) {
             if (EnchantmentHelper.get(itemStack).containsKey(Enchantments.MENDING)) {
                 return Items.AIR;
             }
