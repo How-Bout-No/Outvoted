daba017faf46e833c42d2371002777bb59272785
HowBoutNo <thefalconpunch@icloud.com>
2021-10-27 21:30:11 -0400

    Fixes #98

--- forge/src/main/java/io/github/how_bout_no/outvoted/forge/data/Recipes.java
+++ forge/src/main/java/io/github/how_bout_no/outvoted/forge/data/Recipes.java
@@ -1,14 +1,12 @@
 package io.github.how_bout_no.outvoted.forge.data;
 
+import io.github.how_bout_no.outvoted.init.ModBlocks;
 import io.github.how_bout_no.outvoted.init.ModItems;
 import io.github.how_bout_no.outvoted.init.ModRecipes;
 import net.minecraft.block.Blocks;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.server.RecipesProvider;
-import net.minecraft.data.server.recipe.ComplexRecipeJsonFactory;
-import net.minecraft.data.server.recipe.RecipeJsonProvider;
-import net.minecraft.data.server.recipe.ShapedRecipeJsonFactory;
-import net.minecraft.data.server.recipe.SmithingRecipeJsonFactory;
+import net.minecraft.data.server.recipe.*;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
 
@@ -52,5 +50,21 @@ public class Recipes extends RecipesProvider {
                 .offerTo(consumer, ModRecipes.REPAIR_COST.getId().getPath());
         ComplexRecipeJsonFactory.create(ModRecipes.SHIELD_DECO.get())
                 .offerTo(consumer, ModRecipes.SHIELD_DECO.getId().getPath());
+        ShapelessRecipeJsonFactory.create(ModBlocks.COPPER_BUTTON.get())
+                .input(Blocks.COPPER_BLOCK)
+                .criterion("has_copper", conditionsFromItem(Blocks.COPPER_BLOCK))
+                .offerTo(consumer);
+        ShapelessRecipeJsonFactory.create(ModBlocks.EXPOSED_COPPER_BUTTON.get())
+                .input(Blocks.EXPOSED_COPPER)
+                .criterion("has_ecopper", conditionsFromItem(Blocks.EXPOSED_COPPER))
+                .offerTo(consumer);
+        ShapelessRecipeJsonFactory.create(ModBlocks.WEATHERED_COPPER_BUTTON.get())
+                .input(Blocks.WEATHERED_COPPER)
+                .criterion("has_wcopper", conditionsFromItem(Blocks.WEATHERED_COPPER))
+                .offerTo(consumer);
+        ShapelessRecipeJsonFactory.create(ModBlocks.OXIDIZED_COPPER_BUTTON.get())
+                .input(Blocks.OXIDIZED_COPPER)
+                .criterion("has_ocopper", conditionsFromItem(Blocks.OXIDIZED_COPPER))
+                .offerTo(consumer);
     }
 }
